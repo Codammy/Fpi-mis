ee9ad8b24dc425ef5b68b83847e88a33af79e4ca
Codam <damisco005@gmail.com>
2023-08-31 12:40:52 +0100

    changes from my android

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-http = require('http');
+ http = require('http');
+fs = require('fs')
 http.createServer((req, res)=>{
     res.writeHead(200, {'Content-type': 'text/html'})
-    res.end("Hello User")
-}).listen(8080)
+console.log('request sent')
+	fs.readFile('index.html', (err, buff)=>{
+	if (err)
+		console.log('file does\'nt exist')
+	res.write('<head><link rel="stylesheet" href="style1.css"></head>')
+	res.write(buff)
+	console.log(req.url, req.method)
+	res.end()
+	})
+}).listen(8080, 'localhost', ()=>console.log('server started at port 8080 and awaiting request'))
