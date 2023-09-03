ca5602adb74110359ded5f21c243de63ac281774
Codam <damisco005@gmail.com>
2023-09-03 17:59:53 +0100

    new push

--- server.js
+++ server.js
@@ -1,84 +1,50 @@
 const http = require('http')
 const fs = require('fs')
+console.log(__filename)
 function backend(req, res) {
 	res.writeHead(200, { 'Content-type': 'text/html' })
 
-	// const path = '.' + req.url;
-	// fs.readFile(path, (err, data)=>{
-	// 	if (err)
-	// 		console.log("wrong file path or file doesn't. " + path);
-	// 	else
-	// 		{
-	// 			res.write(data)
-	// 			console.log('requested ' + req.url);
-	// 		}
-	// 		res.end()
-	// })
-	const path = req.url;
-	switch (path) {
-		case '/':
-			fs.readFile('./index.html', (err, data) => {
-				if (err)
-					console.log("wrong file path or file doesn't. exist" + path);
-				else {
-					res.write(data)
-					console.log('requested ' + req.url);
-				}
-				res.end()
-			})
-			break;
+	let path = '.';
+	switch (req.url) {
 		case '/admin-login':
-			fs.readFile('admin/login.html', (err, data) => {
-				if (err)
-					console.log("wrong file path or file doesn't. exist " + path);
-				else {
-					res.write(data)
-					console.log('requested ' + req.url);
-				}
-				res.end()
-			})
+			path += '/Admin/login.html'
 			break;
 		case '/student-login':
-			fs.readFile('student/login.html', (err, data) => {
-				if (err)
-					console.log("wrong file path or file doesn't. exist " + path);
-				else {
-					res.write(data)
-					console.log('requested ' + req.url);
-				}
-				res.end()
-			})
+			path += '/Student/login.html'
 			break;
 		case '/admin':
-			fs.readFile('admin/admin.html', (err, data) => {
-				if (err)
-					console.log("wrong file path or file doesn't. exist " + path);
-				else {
-					res.write(data)
-					console.log('requested ' + req.url);
-				}
-				res.end()
-			})
+			path += '/Admin/admin.html'
 			break;
 		case '/student':
-			fs.readFile('student/student.html', (err, data) => {
-				if (err)
-					console.log("wrong file path or file doesn't. exist " + path);
-				else {
-					res.write(data)
-					console.log('requested ' + req.url);
-				}
-				res.end()
-			})
+			path += 'Student/student.html'
 			break;
-		case 'login':
-			res.setHeader({'Location': '/student-login'})// REDIRECT NOT FUNCTIONAL
-			break;
-		default:
-			res.write(`<h1>${path}<br />404 PAGE NOT FOUND</h1>`)
+		case '/login':
+			res.writeHead(301, {'Location': '/student-login'})// REDIRECT NOT FUNCTIONAL
+			res.statusCode = 301
 			res.end()
 			break;
+		default:
+			if (req.url == '/')
+				path += '/index.html'
+			else
+				path += req.url;
+			break;
 	}
+	fs.readFile(path, (err, data) => {
+		if (err)
+		{
+			console.log("wrong file path or file doesn't. exist " + path);
+			res.write(`<h1>${req.url}<br />404 PAGE NOT FOUND</h1>`)
+			res.statusCode = 404
+			res.end()
+		}
+		else {
+			res.write(data)
+			console.log('requested ' + req.url);
+		}
+		console.log(res.statusCode)
+		res.end()
+	})
 }
 const server = http.createServer(backend)
 server.listen(3000, 'localhost', () => console.log("Server started... "))
